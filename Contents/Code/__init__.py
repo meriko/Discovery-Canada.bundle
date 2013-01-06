3afca2d99388b0faea2ee105b6445392738d06c6
sander1 <sander@plexapp.com>
2013-01-06 15:35:09 +0100

    Rename icon/art; add @handler.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,64 +1,67 @@
-####################################################################################################
-TITLE = "Discovery Channel Canada"
-ART = 'art-discovery.jpg'
-ICON = 'icon-discovery.png'
+TITLE = 'Discovery Channel Canada'
+ART = 'art-default.jpg'
+ICON = 'icon-default.png'
 NAMESPACES = {'ctv': 'http://www.ctv.ca'}
 
 # iOS app video feed, must be parsed manually
 # They say: "This bin contains all the videos that will be picked up by the iOS Discovery app. Show specific clips will be filtered using video/clip title field."
-FEED_URL="http://www.discovery.ca/feeds/videos.aspx" 
+FEED_URL = 'http://www.discovery.ca/feeds/videos.aspx'
 
 # show listings as served for the iOS app (and assuming other things on their site)
-SHOWS_URL="http://www.discovery.ca/feeds/shows.aspx"
-
+SHOWS_URL = 'http://www.discovery.ca/feeds/shows.aspx'
 
 ##########################################################################################
 def Start():
-	Plugin.AddPrefixHandler("/video/discoveryca", MainMenu, TITLE, ICON, ART)
+
 	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
 
 	# Setup the default attributes for the ObjectContainer
 	ObjectContainer.title1 = TITLE
 	ObjectContainer.view_group = 'List'
 	ObjectContainer.art = R(ART)
-	
+
 	# Setup the default attributes for the other objects
 	DirectoryObject.thumb = R(ICON)
 	DirectoryObject.art = R(ART)
-	EpisodeObject.thumb = R(ICON)
-	EpisodeObject.art = R(ART)
+	VideoClipObject.thumb = R(ICON)
+	VideoClipObject.art = R(ART)
 
 	HTTP.CacheTime = CACHE_1HOUR
 
 ##########################################################################################
+@handler('/video/discoveryca', TITLE, art=ART, thumb=ICON)
 def MainMenu():
+
 	return GetShowList()
 
+##########################################################################################
 @route("/video/discoveryca/getshowlist")
 def GetShowList():
- 	oc = ObjectContainer()
+
+	oc = ObjectContainer()
 	data = XML.ObjectFromURL(SHOWS_URL)
 	shows = data.xpath("//item/includeInApp[text()='True']/ancestor::item")
+
 	for show in shows:
 		try:
-			title=show.xpath("./title/text()", namespaces=NAMESPACES)[0]
-			summary=show.xpath("./ctv:summary/text()", namespaces=NAMESPACES)[0]
+			title = show.xpath("./title/text()", namespaces=NAMESPACES)[0]
+			summary = show.xpath("./ctv:summary/text()", namespaces=NAMESPACES)[0]
+
 			if Client.Platform == 'MacOSX' or Client.Platform == 'Windows' or Client.Platform == 'Linux':
 				# header/banner style image
-				thumb_url=show.xpath("./ctv:images/ctv:image[1]/url/text()", namespaces=NAMESPACES)[0]
+				thumb_url = show.xpath("./ctv:images/ctv:image[1]/url/text()", namespaces=NAMESPACES)[0]
 			else:
 				# square thumb style image
-				thumb_url=show.xpath("./ctv:images/ctv:image[2]/url/text()", namespaces=NAMESPACES)[0]
+				thumb_url = show.xpath("./ctv:images/ctv:image[2]/url/text()", namespaces=NAMESPACES)[0]
 
-			showTypeId=show.xpath("./showTypeId/text()")[0]
-			oc.add(
-				DirectoryObject(
-					key=Callback(GetEpisodeList, showTypeId=showTypeId), 
-					title=title,
-					summary=summary, 
-					thumb=Resource.ContentsOfURLWithFallback(url=thumb_url, fallback=ICON)
-				)
-			)
+			showTypeId = show.xpath("./showTypeId/text()")[0]
+
+			oc.add(DirectoryObject(
+				key = Callback(GetEpisodeList, title=title, showTypeId=showTypeId),
+				title = title,
+				summary = summary,
+				thumb = Resource.ContentsOfURLWithFallback(url=thumb_url, fallback=ICON)
+			))
 		except:
 			# no showTypeId means we have no videos for this so let's pass
 			pass
@@ -67,10 +70,12 @@ def GetShowList():
 
 ##########################################################################################
 @route("/video/discoveryca/getepisodelist")
-def GetEpisodeList(showTypeId):
-	oc = ObjectContainer()
+def GetEpisodeList(title, showTypeId):
+
+	oc = ObjectContainer(title2=title)
 	data = XML.ObjectFromURL(FEED_URL)
 	shows = data.xpath('//item/ctv:videoType[text()="%s"]/ancestor::item' % showTypeId, namespaces=NAMESPACES)
+
 	for ep in shows:
 		try:
 			title = ep.xpath("./title/text()")[0]
@@ -79,18 +84,19 @@ def GetEpisodeList(showTypeId):
 			thumb_url = ep.xpath("./imgUrl/text()")[0]
 			summary = ep.xpath("./description/text()")[0]
 			originally_available_at = Datetime.ParseDate(ep.xpath("./ctv:startDate/text()", namespaces=NAMESPACES)[0]).date()
+
 			oc.add(VideoClipObject(
 				title = title,
 				url = url, 
 				thumb = Resource.ContentsOfURLWithFallback(url=thumb_url, fallback=ICON), 
 				summary = summary, 
 				originally_available_at = originally_available_at
-				)
-			)
+			))
 		except:
 			# some shows don't have clipList items set, in that case they are no good to us
 			pass
+
 	if len(oc) < 1:
-		return MessageContainer("Sorry", "There aren't any videos currently available for this show.")
+		return ObjectContainer(header="Sorry", message="There aren't any videos currently available for this show.")
 
 	return oc
